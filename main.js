905ab2f83d436aa92fd113762bd96e8d03d637c9
eazy504 <Ethanbenoit2@gmail.com>
2025-06-21 19:39:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Initialize map
 const map = L.map('map').setView([30, 10], 2);
 
-// Base map (natural Earth style, fully visible now)
+// Base map (English map tiles)
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
   maxZoom: 18,
   attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>'
